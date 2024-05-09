0ac1830e463421181f705b043cf5a6818c6e19b2
RememberIOm <rememberiom@icloud.com>
2024-05-09 20:52:54 +0900

    compile_and_test.sh 컴파일한 o 파일을 obj 디렉토리에 저장하도록 수정

--- quorum/quorumC/complie_and_test.sh
+++ quorum/quorumC/complie_and_test.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# 현재 폴더에 있는 모든 C 파일 컴파일
-gcc -c *.c
+# 오브젝트 파일을 저장할 디렉토리 생성 (이미 존재하는 경우 무시)
+mkdir -p obj
+
+# 현재 폴더에 있는 모든 C 파일 컴파일, 오브젝트 파일은 obj 디렉토리에 저장
+for file in *.c
+do
+    gcc -c "$file" -o "obj/$(basename "$file" .c).o"
+done
 
 # 컴파일된 모든 o 파일로 라이브러리 파일 생성
-ar rcs libquorum.a *.o
+ar rcs libquorum.a obj/*.o
 
 # 2회 상위 디렉토리로 이동
 cd ../..
